3890169d35cc59c6ced6cd9c7f0d8eb1b7289bb0
Sandro0330 <60124712+Sandro0330@users.noreply.github.com>
2020-06-21 14:16:15 -0300

    Limitando calculadora (sem letra e sem pontos duplos)

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,18 +1,52 @@
 package gui;
 
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert.AlertType;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
-public class ViewController {
+public class ViewController implements Initializable {
 
 	@FXML
-	private Button btTest;
-	
+	private TextField txtNum1;
+
 	@FXML
-	public void onBtTestAction() {
-		Alerts.showAlert("Titulo do alerta", null, "Boa noite !!", AlertType.WARNING);
+	private TextField txtNum2;
+
+	@FXML
+	private Label lblResultado;
+
+	@FXML
+	private Button btnSoma;
+
+	@FXML
+	public void onBtnSomaAction() {	
+		try {
+			
+			Locale.setDefault(Locale.US);
+			double numero1 = Double.parseDouble(txtNum1.getText());
+			double numero2 = Double.parseDouble(txtNum2.getText());
+			double soma = numero1 + numero2;
+			lblResultado.setText(String.format("%.2f", soma));	
+		}
+		catch (NumberFormatException e) {
+			Alerts.showAlert("Erro", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
-	
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Constraints.setTextFieldDouble(txtNum1);
+		Constraints.setTextFieldDouble(txtNum2);
+		
+	}
+
 }
